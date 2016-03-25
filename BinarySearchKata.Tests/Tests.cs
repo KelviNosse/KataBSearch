4679ad18dffd8cf58d7618f8877815d3be866ab4
Kelvin Nose <kelvinjosse@gmail.com>
2016-03-24 20:15:40 -0600

    Adding Unit Tests for BSearch Check if Chop is sequential

---
+++ BinarySearchKata.Tests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinarySearchKata.Logic;
+
+namespace BinarySearchKata.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        
+        [TestMethod]
+        public void Check_Chop_Sequential()
+        {
+            
+           int[] numbers = new[] {10, 23, 4, 3, 5, 9, 80};  
+           var bsearch = new BinarySearch();
+           var result = bsearch.Chop_Sequential(5, numbers);
+           Assert.AreEqual(2, result, "Expected index 2 when bsearch for 5 lol");
+        }
+    }
+}
