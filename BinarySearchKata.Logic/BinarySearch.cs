36d7821b2eea7633792414f1772d9368bd537e63
Kelvin Nose <kelvinjosse@gmail.com>
2016-03-24 20:14:18 -0600

    Adding Logic here, Bsearch Class Library with corresponding function

---
+++ BinarySearchKata.Logic/BinarySearch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchKata.Logic
+{
+    public class BinarySearch
+    {
+
+        public int Chop_Sequential(int number, int[] arrNumbers)
+        {
+            Array.Sort(arrNumbers);
+            int length = arrNumbers.Length;
+            int min = 0;
+            int max = length-1;
+            int mid;
+
+            while (min <= max)
+            {
+                mid = (max + min)/2;
+                if (arrNumbers[mid] == number)
+                    return mid;
+                else
+                {
+                    if (number < arrNumbers[mid])
+                    {
+                        max = mid - 1;
+                    }
+                    else
+                    {
+                        min = mid + 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
